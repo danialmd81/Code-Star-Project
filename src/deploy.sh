f9fad037d961d6ac323d04f40dead3627abdbd59
Danial Mobini <danialmobinidh81@gmail.com>
2025-08-21 19:04:41 +0330

    Refactor Docker Compose configurations to improve clarity, add database initialization scripts, and update SSL certificate handling using Docker secrets.

--- src/deploy.sh
+++ src/deploy.sh
@@ -2,9 +2,9 @@ docker stack deploy \
 	-c docker-compose.yml \
 	-c services/database/docker-compose.yml \
 	-c services/keycloak/docker-compose.yml \
-	-c services/monitoring/docker-compose.yml \
 	-c services/nginx/docker-compose.yml \
-	-c services/spark/docker-compose.yml \
 	etl-project
-	
-	# -c services/frontend/docker-compose.yml \
+
+# -c services/spark/docker-compose.yml \
+# -c services/monitoring/docker-compose.yml \
+# -c services/frontend/docker-compose.yml \
