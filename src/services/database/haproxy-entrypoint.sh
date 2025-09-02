bfa8f039abbeb64ce78b5dcc0c212cae81fdcd06
Danial Mobini <danialmobinidh81@gmail.com>
2025-09-02 22:03:29 +0330

    fix: Enhance HAProxy entrypoint script to wait for PostgreSQL replica readiness

--- src/services/database/haproxy-entrypoint.sh
+++ src/services/database/haproxy-entrypoint.sh
@@ -3,4 +3,10 @@ until nc -z pg-0 5432; do
 	echo "Waiting for PostgreSQL primary..."
 	sleep 2
 done
+
+until nc -z pg-1 5432; do
+	echo "Waiting for PostgreSQL replica..."
+	sleep 2
+done
+
 exec haproxy -f /usr/local/etc/haproxy/haproxy.cfg
