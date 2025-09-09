f5a79fdbffb96dbbd2f6fa762187552e37f801ba
Danial Mobini <danialmobinidh81@gmail.com>
2025-09-10 01:46:50 +0330

    feat: Add wait-for-Spark Master functionality to the Keycloak startup script

--- src/services/nginx/wait-for-keycloak.sh
+++ src/services/nginx/wait-for-keycloak.sh
@@ -3,4 +3,10 @@ until nc -z keycloak 8080; do
 	echo "Waiting for Keycloak at keycloak:8080..."
 	sleep 2
 done
+
+until nc -z spark-master 7077; do
+	echo "Waiting for Spark Master at spark-master:7077..."
+	sleep 2
+done
+
 exec nginx -g 'daemon off;'
